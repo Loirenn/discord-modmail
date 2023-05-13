b38c335dabaa8bd1adafe42fc4f7d5932441379d
Loiren <84875764+Loirenn@users.noreply.github.com>
2023-05-13 16:29:06 +0300

    Update config.ts

--- config.ts
+++ config.ts
@@ -2,7 +2,7 @@ export default {
     token: '', // Botunuzun tokeni
     mongoURI: '', // MongoDB bağlantı URL'niz
     modmails: {
-        parent: '', // Kanalına açılacağı kategori ID'si
+        parent: '', // Kanalın açılacağı kategori ID'si
         guild: '' // Sunucu ID'si
     }
 };
